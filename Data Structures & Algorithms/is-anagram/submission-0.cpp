615906375189d9f892c2b75aa87a9970252f1dda
Slobodskoy <slobodskoy.a.v@yandex.ru>
2026-08-05 17:25:48 +0300

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+         vector<int> counter(26);
+         for (const auto ch: s) {
+            counter[ch-'a']++;
+         }
+         for (const auto ch: t) {
+            counter[ch-'a']--;
+         }
+         for (const auto c: counter) {
+            if (c!=0) {
+                return false;
+            }
+         }
+
+         return true;
+    }
+};
