3d2c5719d1e72d4009e159cf11aa9f1e4174887f
Slobodskoy <slobodskoy.a.v@yandex.ru>
2026-08-06 22:37:38 +0300

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+   public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_set<char> set;
+        int l = 0, r = 0, result = 0;
+        while (r < s.size()) {
+            if (set.contains(s[r])) {
+                result = max(result, r - l);
+                while (l <= r && s[l] != s[r]) {
+                    set.erase(s[l]);
+                    l++;
+                }
+                set.erase(s[l]);
+                l++;
+            }
+            set.insert(s[r]);
+            r++;
+        }
+
+        return result;
+    }
+};
