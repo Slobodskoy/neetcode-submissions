dd89a58f312da33a93f3c6f9dd9afc53763ef2ea
Slobodskoy <slobodskoy.a.v@yandex.ru>
2026-08-06 22:44:55 +0300

    Add: max-water-container - submission-2

---
+++ Data Structures & Algorithms/max-water-container/submission-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxArea(vector<int>& heights) {
+        int totalMax = 0;
+        int l = 0, r = heights.size()-1;
+        while (l<r) {
+            int localMax = min(heights[l], heights[r]) * (r-l);
+            totalMax = max(totalMax, localMax);
+            if (heights[l]<heights[r]) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+
+        return totalMax;
+    }
+};
